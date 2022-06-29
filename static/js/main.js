cbce7068feabe6e7b76f034417549858c52a2b86
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-06-29 18:54:43 +0000

    Added plus minus input buttons

--- static/js/main.js
+++ static/js/main.js
@@ -216,18 +216,6 @@
     });
 
     /*==================================================================
-    [ +/- num product ]*/
-    $('.btn-num-product-down').on('click', function () {
-        var numProduct = Number($(this).next().val());
-        if (numProduct >= 2) $(this).next().val(numProduct - 1);
-    });
-
-    $('.btn-num-product-up').on('click', function () {
-        var numProduct = Number($(this).prev().val());
-        $(this).prev().val(numProduct + 1);
-    });
-
-    /*==================================================================
     [ Rating ]*/
     $('.wrap-rating').each(function () {
         var item = $(this).find('.item-rating');
