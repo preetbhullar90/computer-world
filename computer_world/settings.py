09ccdfc358454919b0c867bf1958ccb61bd5db45
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-07-13 15:14:06 +0000

    056 Deployment part 2

--- computer_world/settings.py
+++ computer_world/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-uv(#w&s3qp+g7fs=18!nws4pvv88dv)u&mp!1_pvy@hgzpzcep
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['preet-computer-world.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'computer_world.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
