2622f3060ef9eb081b2f5a0628c3271b5ccb629f
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-07-14 00:04:57 +0000

    Removed secret key

--- computer_world/settings.py
+++ computer_world/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uv(#w&s3qp+g7fs=18!nws4pvv88dv)u&mp!1_pvy@hgzpzcep'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['preet-computer-world.herokuapp.com', 'localhost']
 
