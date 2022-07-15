db96e6f16fd24a0d07fc22663bd27bed4896885f
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-07-15 15:32:13 +0000

    Added cache control

--- computer_world/settings.py
+++ computer_world/settings.py
@@ -183,8 +183,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Bucket Config
+
+
+
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'preet-computer-world'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
