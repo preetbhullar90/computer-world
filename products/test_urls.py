6cb40142b134553a0a08ac3bec9c63c8060413ee
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-09-17 17:39:04 +0000

    Added test_urls and test_view for Add product, Edit product and delete product for superuser in products app

--- products/test_urls.py
+++ products/test_urls.py
@@ -1,6 +1,8 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from products.views import all_products, product_detail, submit_review, edit_review, delete_review
+from products.views import (all_products, product_detail, add_product,
+                            edit_product, delete_product, submit_review,
+                            edit_review, delete_review)
 
 
 class TestProductUrls(SimpleTestCase):
@@ -11,6 +13,18 @@ class TestProductUrls(SimpleTestCase):
     def test_product_detail_url_is_resolved(self):
         url = reverse('product_detail', args=['1'])
         self.assertEquals(resolve(url).func, product_detail)
+
+    def test_add_product_url_is_resolved(self):
+        url = reverse('add_product')
+        self.assertEquals(resolve(url).func, add_product)
+
+    def test_edit_product_url_is_resolved(self):
+        url = reverse('edit_product', args=[1])
+        self.assertEquals(resolve(url).func, edit_product)
+
+    def test_delete_product_url_is_resolved(self):
+        url = reverse('delete_product', args=[3])
+        self.assertEquals(resolve(url).func, delete_product)
 
     def test_submit_review_url_is_resolved(self):
         url = reverse('submit_review', args=[3])
--- products/test_views.py
+++ products/test_views.py
@@ -9,6 +9,9 @@ class TestProductsViews(TestCase):
         self.client = Client()
         self.category_url = reverse('product_detail', args=['1'])
         self.product_url = reverse('products')
+        self.product_add_url = reverse('add_product')
+        self.product_edit_url = reverse('edit_product', args=['1'])
+        self.product_delete_url = reverse('delete_product', args=['5'])
         self.submit_review_url = reverse('submit_review', args=['3'])
         self.edit_review_url = reverse('edit_review', args=['1'])
         self.delete_review_url = reverse('delete_review', args=['1'])
@@ -50,6 +53,18 @@ class TestProductsViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/products.html')
 
+    def test_product_add_url_GET(self):
+        response = self.client.get(self.product_add_url)
+        self.assertEquals(response.status_code, 302)
+
+    def test_product_edit_url_GET(self):
+        response = self.client.get(self.product_edit_url)
+        self.assertEquals(response.status_code, 302)
+
+    def test_product_delete_url_GET(self):
+        response = self.client.get(self.product_delete_url)
+        self.assertEquals(response.status_code, 302)
+
     def test_submit_review_GET(self):
         response = self.client.get(self.submit_review_url)
         self.assertEquals(response.status_code, 404)
@@ -68,6 +83,18 @@ class TestProductsViews(TestCase):
 
         response = self.client.post(self.product_url)
         self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(Product.objects.all()), 1)
+
+        response = self.client.post(self.product_add_url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(len(Product.objects.all()), 1)
+
+        response = self.client.post(self.product_edit_url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(len(Product.objects.all()), 1)
+
+        response = self.client.post(self.product_delete_url)
+        self.assertEquals(response.status_code, 302)
         self.assertEquals(len(Product.objects.all()), 1)
 
         response = self.client.post(self.submit_review_url)
