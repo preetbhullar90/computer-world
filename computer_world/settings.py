f7d8b446fca8df1aaf162f402132867172fa5172
preetbhullar90 <gurpreet.singhb90@gmail.com>
2023-05-22 06:29:33 +0000

    add setting for heroku

--- computer_world/settings.py
+++ computer_world/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+
 ALLOWED_HOSTS = ['preet-computer-world.herokuapp.com', 'localhost']
 
 # Add Render.com URL to allowed hosts
